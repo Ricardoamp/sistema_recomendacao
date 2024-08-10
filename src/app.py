62127c203d7e19fdbae46cc9eaf393c07311e0b6
Ricardo <ricardo.amp12@gmail.com>
2024-08-10 15:47:28 -0300

    Testando melhorias

--- src/app.py
+++ src/app.py
@@ -9,8 +9,15 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+parser_ps = PorterStemmer()
+
+# Função para carregar os dados com cache para melhorar o desempenho
+@st.cache_data
+def carregar_dados():
+    return pd.read_csv('src/amazon.csv')
+
 # Carregar os dados
-df_raw = pd.read_csv('src/amazon.csv')
+df_raw = carregar_dados()
 
 # Função stemming
 def stem(text):
@@ -23,37 +30,28 @@ def stem(text):
 def sistema_recomendacao(movie):
     index = df7[df7['product_name'] == movie].index[0]
     distances = sorted(list(enumerate(similaridades[index])), reverse=True, key=lambda x: x[1])
-    recommended_products = [df7.iloc[i[0]].product_name for i in distances[1:6]]
+    recommended_products = [df7.iloc[i[0]].product_name for i in distances[1:4]]  # Exibir apenas 3 recomendações
     return recommended_products
 
 # Preparação dos dados
-df1 = df_raw.copy()
-df2 = df1[['product_id', 'product_name', 'category', 'about_product', 'review_title', 'review_content']]
-df3 = df2.drop_duplicates()
+df1 = df_raw[['product_id', 'product_name', 'category', 'about_product', 'review_title', 'review_content']].drop_duplicates()
 
-df4 = df3.copy()
-df4['category'] = df4['category'].apply(lambda x: x.split())
-df4['about_product'] = df4['about_product'].apply(lambda x: x.split())
-df4['review_title'] = df4['review_title'].apply(lambda x: x.split())
-df4['review_content'] = df4['review_content'].apply(lambda x: x.split())
+df1['tags'] = (df1['category'] + ' ' + df1['about_product'] + ' ' + df1['review_title'] + ' ' + df1['review_content']).apply(lambda x: x.split())
 
-df4['tags'] = df4['category'] + \
-              df4['about_product'] + \
-              df4['review_title'] + \
-              df4['review_content']
+df1['tags'] = df1['tags'].apply(lambda x: " ".join(x).lower())
+df1['tags'] = df1['tags'].apply(stem)
 
-df4 = df4[['product_id', 'product_name', 'tags']]
-df4['tags'] = df4['tags'].apply(lambda x: " ".join(x))
-df4['tags'] = df4['tags'].apply(lambda x: x.lower())
+# Função para calcular as similaridades com cache
+@st.cache_resource
+def calcular_similaridades(vectors):
+    return cosine_similarity(vectors)
 
-parser_ps = PorterStemmer()
-df4['tags'] = df4['tags'].apply(stem)
-
+# Vectorização e cálculo das similaridades
 cv = CountVectorizer(max_features=5000, stop_words='english')
-vectors = cv.fit_transform(df4['tags']).toarray()
+vectors = cv.fit_transform(df1['tags']).toarray()
+similaridades = calcular_similaridades(vectors)
 
-similaridades = cosine_similarity(vectors)
-df7 = df4.copy()
+df7 = df1.copy()
 
 # Configurar o aplicativo Streamlit
 st.title('Sistema de Recomendação de Produtos')
